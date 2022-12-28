33906e376f58d39c7b1400bf407d222358821f54
jzhou077 <97851897+jzhou077@users.noreply.github.com>
2022-12-28 11:19:29 -0500

    homepage v2

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ $(document).ready(function() {
     $(".contactBtn").click(function () {
         $(".modal").css("visibility", "visible");
         $(".modal").css("opacity", "1");
+        $(".overlay").css("display", "block");
+        $(".overlay").css("opacity", "0.25");
     });
     $(".modalCloseBtn").click(function(){
         $(".modal").css("opacity", "0");
         $(".modal").css("visibility", "hidden");
+        $(".overlay").css("opacity", "0");
+        $(".overlay").css("display", "none");
+    });
+    $(".overlay").click(function () {
+        $(".modal").css("opacity", "0");
+        $(".modal").css("visibility", "hidden");
+        $(".overlay").css("opacity", "0");
+        $(".overlay").css("display", "none");
     });
 });
